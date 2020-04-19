ecbeac7bc331f18893d0670a2a47f21e0721040d
Brian <haack79@hotmail.com>
2020-04-19 11:58:00 -0700

    added test xunit

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -1,5 +1,8 @@
 using System;
 using Xunit;
+// xunit above needed for [Fact] attribute and testing, 
+// attributes are methods in a class  to help do specific tasks,
+// [Fact] here lets us run specific items we want to run tests on.
 
 namespace GradeBook.Tests
 {
